3bdccf0e427aee42c43711e7cc6644308e406973
Lucas <lucasbsb100@gmail.com>
2022-01-31 20:05:06 -0300

    Add Themes to project

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class AppColor{
+  static const background = Color(0xFF000000);
+  static const primary = Color(0xFFe60911);
+  static const secundary = Color(0xFFFFFFFF);
+  static const terciary = Color(0xFF757575);
+}
---
+++ lib/themes/app_text_styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final subTitleText = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static final headerText = GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static final userNameText = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static final listTitleText = GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static final bottomNavBarText = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xFF757575),
+  );
+  static final titleMovieText = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+  static final descriptionMovieText = GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xFF757575),
+  );
+}
