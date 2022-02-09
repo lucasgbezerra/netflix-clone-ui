7a0a39ea46c52a5b66b9767dab0a2dce06d61171
Lucas <lucasbsb100@gmail.com>
2022-02-08 23:36:18 -0300

    Add List of movies on top search

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_clone_ui/provider/data.dart';
 import 'package:netflix_clone_ui/themes/app_colors.dart';
 import 'package:netflix_clone_ui/themes/app_text_styles.dart';
+import 'package:netflix_clone_ui/widgets/search_movie_tile.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -9,6 +12,7 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: AppColor.background,
       appBar: AppBar(
         backgroundColor: AppColor.background,
         leading: BackButton(
@@ -34,25 +38,57 @@ class SearchScreen extends StatelessWidget {
           child: TextField(
             style: AppTextStyles.searchLabelText,
             cursorColor: Colors.white,
+            cursorHeight: 20,
             decoration: InputDecoration(
-                hintText: "Search for a movie, genre, etc.",
-                hintStyle: AppTextStyles.searchHintText,
-                focusedBorder: InputBorder.none,
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.grey[300],
-                ),
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.mic),
-                  color: Colors.grey[300],
-                ),
-                filled: true,
-                fillColor: AppColor.terciary),
+              hintText: "Search for a movie, genre, etc.",
+              hintStyle: AppTextStyles.searchHintText,
+              focusedBorder: InputBorder.none,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey[300],
+              ),
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.mic),
+                color: Colors.grey[300],
+              ),
+              filled: true,
+              fillColor: AppColor.terciary,
+              border: InputBorder.none,
+            ),
           ),
         ),
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.only(top: 5),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                child: Text(
+                  "Top Searchs",
+                  style: GoogleFonts.roboto(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                      
+                    separatorBuilder: (context, index) => SizedBox(height: 5),
+                    itemCount: Data.moviesAndTvShowInfo.length,
+                    itemBuilder: (context, index) {
+                      return SearchMovieTile(Data.moviesAndTvShowInfo[index]);
+                    },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
