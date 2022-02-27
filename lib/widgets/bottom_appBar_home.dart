f8821ba7ca90ab9e7b0c1205d691d799c46fb687
Lucas <lucasbsb100@gmail.com>
2022-02-26 22:48:16 -0300

    Add Category list from api

--- lib/widgets/bottom_appBar_home.dart
+++ lib/widgets/bottom_appBar_home.dart
@@ -4,9 +4,15 @@ import 'package:netflix_clone_ui/screens/category_screen.dart';
 import 'package:netflix_clone_ui/themes/app_colors.dart';
 import 'package:netflix_clone_ui/themes/app_text_styles.dart';
 
-class BottomAppBarHome extends StatelessWidget {
+class BottomAppBarHome extends StatefulWidget {
   const BottomAppBarHome({Key? key}) : super(key: key);
 
+  @override
+  State<BottomAppBarHome> createState() => _BottomAppBarHomeState();
+}
+
+class _BottomAppBarHomeState extends State<BottomAppBarHome> {
+  int categoryId = -1;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -27,8 +33,13 @@ class BottomAppBarHome extends StatelessWidget {
           ),
         ),
         TextButton.icon(
-          onPressed: () {
-            Navigator.of(context).push(CategoryScreen());
+          onPressed: () async {
+            final category = await Navigator.of(context)
+                .push(CategoryScreen(selectedCategory: categoryId));
+
+            setState(() {
+              categoryId = category;
+            });
           },
           icon: Text(
             "Categories",
